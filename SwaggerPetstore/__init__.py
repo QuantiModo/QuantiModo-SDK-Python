1e1cf8f5caa623db03889695e6cbee9aeac36fbc
Mike P. Sinn <m@quantimodo.com>
2015-06-25 18:34:06 -0500

    Big Bang

--- SwaggerPetstore/__init__.py
+++ SwaggerPetstore/__init__.py
@@ -1,78 +1,43 @@
-#!/usr/bin/env python
-"""Add all of the modules in the current directory to __all__"""
-import os
+from __future__ import absolute_import
 
-# import models into package
-
+# import models into sdk package
 from .models.connector import Connector
-
 from .models.conversion_step import ConversionStep
-
 from .models.correlation import Correlation
-
+from .models.json_error_response import JsonErrorResponse
+from .models.measurement_set import MeasurementSet
 from .models.measurement import Measurement
-
 from .models.measurement_range import MeasurementRange
-
 from .models.measurement_source import MeasurementSource
-
 from .models.pairs import Pairs
-
 from .models.permission import Permission
-
+from .models.post_correlation import PostCorrelation
 from .models.unit import Unit
-
 from .models.unit_category import UnitCategory
-
 from .models.user import User
-
 from .models.user_token_request import UserTokenRequest
-
 from .models.user_token_successful_response import UserTokenSuccessfulResponse
-
 from .models.user_token_failed_response import UserTokenFailedResponse
-
 from .models.user_token_request_inner_user_field import UserTokenRequestInnerUserField
-
 from .models.user_token_successful_response_inner_user_field import UserTokenSuccessfulResponseInnerUserField
-
+from .models.value_object import ValueObject
 from .models.variable import Variable
-
 from .models.variable_category import VariableCategory
-
 from .models.variable_user_settings import VariableUserSettings
+from .models.variable_new import VariableNew
+from .models.variables_new import VariablesNew
 
-
-# import apis into package
-
-from .connectors_api import ConnectorsApi
-
-from .oauth_api import OauthApi
-
-from .variables_api import VariablesApi
-
-from .administration_api import AdministrationApi
-
-from .user_api import UserApi
-
-from .measurements_api import MeasurementsApi
-
-from .sharing_api import SharingApi
-
-from .correlations_api import CorrelationsApi
-
-from .organizations_api import OrganizationsApi
-
-from .pairs_api import PairsApi
-
-from .units_api import UnitsApi
-
+# import apis into sdk package
+from .apis.connectors_api import ConnectorsApi
+from .apis.variables_api import VariablesApi
+from .apis.oauth_api import OauthApi
+from .apis.user_api import UserApi
+from .apis.measurements_api import MeasurementsApi
+from .apis.correlations_api import CorrelationsApi
+from .apis.connect_api import ConnectApi
+from .apis.organizations_api import OrganizationsApi
+from .apis.pairs_api import PairsApi
+from .apis.units_api import UnitsApi
 
 # import ApiClient
-from .swagger import ApiClient
-
-__all__ = []
-
-for module in os.listdir(os.path.dirname(__file__)):
-  if module != '__init__.py' and module[-3:] == '.py':
-    __all__.append(module[:-3])
+from .api_client import ApiClient
--- setup.py
+++ setup.py
@@ -12,21 +12,29 @@ from setuptools import setup, find_packages
 # Try reading the setuptools documentation:
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = []
+REQUIRES = ["urllib3 >= 1.10", "six >= 1.9", "certifi"]
 
 setup(
     name="SwaggerPetstore",
     version="1.0.0",
     description="QuantiModo",
-    author_email="info@interactuamovil.com",
+    author_email="",
     url="",
     keywords=["Swagger", "QuantiModo"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     long_description="""\
-    QuantiModo makes it easy to retrieve normalized user data from a wide array of devices and applications.\n\n[Learn about QuantiModo](https://quantimo.do) or contact us at [help.quantimo.do](https://help.quantimo.do).\n\n Contact info@quantimo.do for an api key to test the authorization filters.\n * Test User Name: `user`\n * Password: `SuperPassword123!!!`
+    QuantiModo makes it easy to retrieve normalized user data from a wide array of devices and applications.\n\n[Learn about QuantiModo](https://quantimo.do) or contact us at [help.quantimo.do](https://help.quantimo.do).\n\n Create an account at [QuantiModo](https://quantimo.do), sign in, and add some data at https://quantimo.do/connect to try out the API for yourself.  As long as you&#39;re signed in, it will use your browser&#39;s cookie for authentication.  However, client applications must use OAuth2 tokens to access the API.
     """
 )
+
+
+
+
+
+
+
+
 
 
