55eac739945426a15aa7a4dca0a3ac6e02dcd3d1
Mike P. Sinn <m@quantimodo.com>
2015-08-13 21:11:24 -0500

    SDK Update

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
 import sys
 from setuptools import setup, find_packages
+
+NAME = "swagger_client"
+VERSION = "1.0.0"
 
 
 
@@ -12,20 +16,20 @@ from setuptools import setup, find_packages
 # Try reading the setuptools documentation:
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.10", "six >= 1.9", "certifi"]
+REQUIRES = ["urllib3 >= 1.10", "six >= 1.9", "certifi", "python-dateutil"]
 
 setup(
-    name="SwaggerPetstore",
-    version="1.0.0",
+    name=NAME,
+    version=VERSION,
     description="QuantiModo",
-    author_email="foo@example.com",
+    author_email="apm-api-team@veracode.com",
     url="",
     keywords=["Swagger", "QuantiModo"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     long_description="""\
-    QuantiModo makes it easy to retrieve normalized user data from a wide array of devices and applications.\n\n\n[Learn about QuantiModo](https://quantimo.do) or contact us at [help.quantimo.do](https://help.quantimo.do).\n\n\n\n Create an account at [QuantiModo](https://quantimo.do), sign in, and add\nsome data at https://quantimo.do/connect to try out the API for yourself.  As long as you&#39;re signed in, it will use your browser&#39;s cookie for authentication.  However, client applications must use OAuth2 tokens to access the API.
+    QuantiModo makes it easy to retrieve normalized user data from a wide array of devices and applications.\n[Learn about QuantiModo](https://app.quantimo.do) or contact us at [help.quantimo.do](https://help.quantimo.do).\n\n\n\n Create an account at [QuantiModo](https://app.quantimo.do), sign in, and\nadd\n\n\nsome data at https://app.quantimo.do/connect to try out the API for yourself.  As long as you&#39;re signed in, it will use your browser&#39;s cookie for authentication.  However, client applications must use OAuth2 tokens to access the API.
     """
 )
 
